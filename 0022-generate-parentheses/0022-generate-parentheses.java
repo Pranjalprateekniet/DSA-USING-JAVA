08f49643d1069715886c7794117b0f3db24a599d
PRANJAL PRATEEK <pranjalprateekniet@gmail.com>
2026-07-09 18:50:54 +0530

    Time: 1 ms (86.85%), Space: 44.6 MB (56.39%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,20 +1,26 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-        List<String>res=new ArrayList<String>();
-        recurse(res,0,0,"",n);
+        List<String>res=new ArrayList<>();
+        StringBuilder sb=new StringBuilder();
+        func(0,0,n,sb,res);
         return res;
     }
-
-    private void recurse(List<String>res,int left,int right,String s,int n){
-        if(s.length()==n*2){
-            res.add(s);
-            return;
+    private void func(int open,int close,int n,StringBuilder temp,List<String>res){
+        if(open==n && close==n)
+            {
+                res.add(temp.toString());
+                return;
+            }
+        if(open<n){
+            temp.append("(");
+            func(open+1,close,n,temp,res);
+            temp.deleteCharAt(temp.length() - 1);
         }
-        if(left<n){
-            recurse(res,left+1,right,s+"(",n);
+        if(close<open){
+            temp.append(")");
+            func(open,close+1,n,temp,res);
+            temp.deleteCharAt(temp.length() - 1);
         }
-        if(right<left){
-            recurse(res,left,right+1,s+")",n);
-        }
+        return;
     }
 }
