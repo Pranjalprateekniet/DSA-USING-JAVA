fd87a9c40cfe4dd2edef6e3841805a37c37d383d
PRANJAL PRATEEK <pranjalprateekniet@gmail.com>
2026-07-20 23:47:40 +0530

    Time: 0 ms (100%), Space: 42.1 MB (52.28%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,9 +1,14 @@
 class Solution {
+    private HashMap<Integer,Integer>memo=new HashMap<>();
     public int fib(int n) {
         if(n==0)
             return 0;
         if (n==1)
             return 1;
-        return fib(n-1)+fib(n-2);
+        if(memo.containsKey(n))
+            return memo.get(n);
+        int ans=fib(n-1)+fib(n-2);
+        memo.put(n,ans);
+        return ans;
     }
 }
