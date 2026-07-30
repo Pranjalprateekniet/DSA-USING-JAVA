04b96c0c61370244c74f6955b3fc077fe64318af
PRANJAL PRATEEK <pranjalprateekniet@gmail.com>
2026-07-31 00:27:35 +0530

    Time: 5 ms (34.27%), Space: 45.6 MB (50.41%) - LeetHub

---
+++ 0120-triangle/0120-triangle.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int m=triangle.size();
+        
+        int [][] dp=new int[m][m];
+
+        for(int j=0;j<m;j++){
+            dp[m-1][j]=triangle.get(m-1).get(j);
+        }
+
+        for(int i=triangle.size()-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                int down=triangle.get(i).get(j)+dp[i+1][j];
+                int dg=triangle.get(i).get(j)+dp[i+1][j+1];
+                dp[i][j]=Math.min(down,dg);
+            }
+        }
+        return dp[0][0];
+    }
+}
