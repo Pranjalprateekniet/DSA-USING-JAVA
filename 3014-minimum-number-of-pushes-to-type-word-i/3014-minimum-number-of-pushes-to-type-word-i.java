83b32e1676a7856e8a0bd2e04821b8c54ef176e8
PRANJAL PRATEEK <pranjalprateekniet@gmail.com>
2026-07-31 08:58:42 +0530

    Time: 0 ms (100%), Space: 43.1 MB (66.43%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -1,10 +1,7 @@
 class Solution {
     public int minimumPushes(String word) {
         int n=word.length();
-        int c=0;
-        for(int i=0;i<n;i++){
-            c+=i/8+1;
-        }
-        return c;
+        int blocks=n/8;
+        return (blocks*(blocks+1)*4)+(n%8)*(blocks+1);
     }
 }
