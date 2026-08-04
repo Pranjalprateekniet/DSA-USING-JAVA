5be54a9474a9b32ab09e41f8db0e646c5c4b8400
PRANJAL PRATEEK <pranjalprateekniet@gmail.com>
2026-08-04 23:42:11 +0530

    Time: 0 ms (100%), Space: 42.7 MB (57.49%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,21 +1,24 @@
 class Solution {
-    HashMap<Integer,Integer>dp=new HashMap<>();
-    private int func(int arr[],int i,int n){
-        if(i==n-1)
-            return arr[i];
-        if(i>=n)
-            return 0;
-        if(dp.containsKey(i))
-            return dp.get(i);
-        int rob=arr[i]+func(arr,i+2,n);
-        int skip=func(arr,i+1,n);
-        int ans=Math.max(rob,skip);
-        dp.put(i,ans);
-        return ans;
-        
-    }
     public int rob(int[] arr) {
+        int k=1;
         int n=arr.length;
-       return func(arr,0,n);
+        int dp[][]=new int[n][2];
+        for(int rows[]:dp)
+            Arrays.fill(rows,-1);
+        int gain=func(arr,n,0,k,dp);
+        return gain;
+    }
+    private static int func(int arr[],int n,int i,int k,int dp[][]){
+        if(i==n)
+            return 0;
+        if(k==0)
+            return func(arr,n,i+1,1,dp);
+        if(dp[i][k]!=-1)
+            return dp[i][k];
+        int c1=arr[i]+func(arr,n,i+1,0,dp);
+        int c2=func(arr,n,i+1,1,dp);
+        dp[i][k]=Math.max(c1,c2);
+        return dp[i][k];
+
     }
 }
