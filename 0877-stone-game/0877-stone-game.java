4e6b0de31de5798d3250b87a3d93d3bd7ee43cce
PRANJAL PRATEEK <pranjalprateekniet@gmail.com>
2026-08-09 15:08:28 +0530

    Time: 8 ms (25.9%), Space: 47 MB (14.54%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,5 +1,25 @@
 class Solution {
+    public int isyes(int i, int j, int[] piles, int[][] dp) {
+        if (i > j) {
+            return 0;
+        }
+        if (i == j) {
+            return piles[i];
+        }
+        if (dp[i][j] != 0) {
+            return dp[i][j];
+        }
+        int left = piles[i] - isyes(i + 1, j, piles, dp);
+        int right = piles[j] - isyes(i, j - 1, piles, dp);
+
+        dp[i][j] = Math.max(left, right);
+        return dp[i][j];
+    }
+
     public boolean stoneGame(int[] piles) {
-        return true;
+        int[][] dp = new int[piles.length][piles.length];
+        int num = isyes(0, piles.length - 1, piles, dp);
+        if (num >= 0) return true;
+        return false;
     }
 }
