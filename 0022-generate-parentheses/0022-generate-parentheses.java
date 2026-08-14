1b6a893133c13a309f5b33b6bdd3ed74b9771818
PRANJAL PRATEEK <pranjalprateekniet@gmail.com>
2026-08-14 14:09:39 +0530

    Time: 0 ms (100%), Space: 44.2 MB (92.99%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,26 +1,28 @@
 class Solution {
-    public List<String> generateParenthesis(int n) {
-        List<String>res=new ArrayList<>();
-        StringBuilder sb=new StringBuilder();
-        func(0,0,n,sb,res);
-        return res;
-    }
-    private void func(int open,int close,int n,StringBuilder temp,List<String>res){
-        if(open==n && close==n)
-            {
-                res.add(temp.toString());
-                return;
-            }
-        if(open<n){
-            temp.append("(");
-            func(open+1,close,n,temp,res);
-            temp.deleteCharAt(temp.length() - 1);
+    private static void func(int open,int close,int n,StringBuilder temp,List<String>ans){
+        if(open==n && close==n){
+            ans.add(temp.toString());
+            return;
+        }
+        if(open<n)
+        {
+            temp.append('(');
+            func(open+1,close,n,temp,ans);
+            temp.deleteCharAt(temp.length()-1);
         }
         if(close<open){
-            temp.append(")");
-            func(open,close+1,n,temp,res);
-            temp.deleteCharAt(temp.length() - 1);
+            temp.append(')');
+            func(open,close+1,n,temp,ans);
+            temp.deleteCharAt(temp.length()-1);
         }
         return;
+
+
+    }
+    public List<String> generateParenthesis(int n) {
+        List<String>ans=new ArrayList<>();
+        StringBuilder temp=new StringBuilder();
+        func(0,0,n,temp,ans);
+        return ans;
     }
 }
