30d941b468f337185690179898b4ad3010469edb
PRANJAL PRATEEK <pranjalprateekniet@gmail.com>
2026-08-14 14:56:25 +0530

    Time: 1 ms (86.42%), Space: 44.4 MB (20.52%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,22 +1,20 @@
 class Solution {
+    private static void func(int i,int n,int arr[],List<Integer>temp,List<List<Integer>>ans){
+        if(i==n){
+            ans.add(new ArrayList<>(temp));
+            return;
+            }
+        func(i+1,n,arr,temp,ans);
+        temp.add(arr[i]);
+        func(i+1,n,arr,temp,ans);
+        temp.remove(temp.size()-1);
+    }
     public List<List<Integer>> subsets(int[] arr) {
         int n=arr.length;
-        int i=0;
         List<Integer>temp=new ArrayList<>();
         List<List<Integer>>ans=new ArrayList<>();
-        backtrack(arr,n,i,temp,ans);
+        func(0,n,arr,temp,ans);
         return ans;
-
+        
     }
-        private void backtrack(int arr[],int n,int i,List<Integer>temp,List<List<Integer>>ans){
-    if(i==n){
-        ans.add(new ArrayList<>(temp));
-        return;
-    }
-    backtrack(arr,n,i+1,temp,ans);
-    temp.add(arr[i]);
-    backtrack(arr,n,i+1,temp,ans);
-    temp.remove(temp.size()-1);
-    return;
-}
 }
