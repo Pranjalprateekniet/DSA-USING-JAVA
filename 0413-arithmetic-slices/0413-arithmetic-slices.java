6d23eeba5995875c0fc86ce6d56d5f5f9a84e04d
PRANJAL PRATEEK <pranjalprateekniet@gmail.com>
2026-08-24 19:36:17 +0530

    Time: 0 ms (100%), Space: 43.1 MB (77.43%) - LeetHub

---
+++ 0413-arithmetic-slices/0413-arithmetic-slices.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numberOfArithmeticSlices(int[] arr) {
+        int n=arr.length;
+        int sum=0;
+        int curr=0;
+        for(int i=2;i<n;i++){
+            if(arr[i]-arr[i-1]==arr[i-1]-arr[i-2])
+            {
+                curr+=1;
+                sum+=curr;
+
+            }
+            else
+                curr=0;
+        }
+        return sum;
+    }
+}
