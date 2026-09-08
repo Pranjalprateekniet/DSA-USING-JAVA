49330ddfbb3d747fef8473114814670ec318f577
PRANJAL PRATEEK <pranjalprateekniet@gmail.com>
2026-09-08 22:38:50 +0530

    Time: 2 ms (88.65%), Space: 44.8 MB (5.73%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,52 +1,51 @@
 class Solution {
-    private int[] delrow={-1,0,1,0};
-    private int[] delcol={0,1,0,-1};
-    private boolean isValid(int i,int j,int n,int m){
-        if(i<0 || i>=n)
-            return false;
-        if(j<0 || j>=m)
+    private boolean isvalid(int i,int j,int n,int m){
+        if(i<0 || i>=n || j<0 || j>=m)
             return false;
         return true;
     }
     public int orangesRotting(int[][] grid) {
         int n=grid.length;
         int m=grid[0].length;
-        Queue<int[]>q=new LinkedList<>();
-        int time =0;
+        int vis[][]=new int[n][m];
         int total=0;
-        int count=0;
+        int time=0;
+        int c=0;
+        Queue<int[]>q=new LinkedList<>();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]!=0){
+                if(grid[i][j]!=0)
                     total++;
-                }
                 if(grid[i][j]==2){
+                    vis[i][j]=2;
                     q.add(new int[]{i,j});
                 }
             }
         }
         while(!q.isEmpty()){
             int k=q.size();
-            count+=k;
+            c+=k;
             while(k-->0){
-                int[] cell=q.poll();
+                int cell[]=q.poll();
                 int row=cell[0];
                 int col=cell[1];
+                int delrow[]={-1,0,1,0};
+                int delcol[]={0,1,0,-1};
                 for(int i=0;i<4;i++){
                     int newrow=row+delrow[i];
                     int newcol=col+delcol[i];
-                    if(isValid(newrow,newcol,n,m)&& grid[newrow][newcol]==1){
-                        grid[newrow][newcol]=2;
+                    if(isvalid(newrow,newcol,n,m) && (grid[newrow][newcol]==1) && vis[newrow][newcol]==0){
+                        vis[newrow][newcol]=2;
                         q.add(new int[]{newrow,newcol});
                     }
                 }
             }
-            if(!q.isEmpty()){
+            if(!q.isEmpty())
                 time++;
-            }
         }
-        if(total==count)
+        if(total==c)
             return time;
         return -1;
+
     }
 }
